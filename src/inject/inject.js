dd4285f4d12295c80c53e16de8a817c6a7507f7e
Leandro <xupisco@gmail.com>
2018-02-22 15:25:02 -0300

    fixed links sorting

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -15,16 +15,17 @@ chrome.extension.sendMessage({}, function(response) {
 				var comment_author = $(this).parent().parent().parent().parent().parent().find('.author').html() || '---';
 				var comment_score = $(this).parent().parent().parent().parent().parent().find('.score.unvoted').html() || '0 points';
 
-				ar_links.push([parseInt(comment_score.replace(' points', '')), link_id, comment_author, comment_score, $(this).attr('href'), $(this).html()]);
+				ar_links.push([parseInt(comment_score.replace(' points', '')), link_id, comment_author, comment_score, $(this).attr('href'), $(this).html(), $(this).parent().parent().text()]);
 			})
 
+
 			ar_links.sort(function(a, b){
-				return a[0] < b[0]
+				return b[0] - a[0]
 			})
 
 			$('#rlinks_holder').append("<table class='rlinks_table'></table>");
 			$.each(ar_links, function(i, v) {
-				$('.rlinks_table').append("<tr><td><a href='#thing_" + v[1] + "' class='rlinks_author'>#</a> <a href='" + v[4] + "'>" + v[5] + "</a></td><td><span class='rlinks_score'>" + v[3] + "</span></td><td><a href='#thing_" + v[1] + "' class='rlinks_author'>" + v[2] + "</a></td></tr>")
+				$('.rlinks_table').append("<tr><td><a href='#thing_" + v[1] + "' class='rlinks_author'>#</a> <a href='" + v[4] + "' title='" + v[6] + "'>" + v[5] + "</a></td><td><span class='rlinks_score'>" + v[3] + "</span></td><td><a href='#thing_" + v[1] + "' class='rlinks_author'>" + v[2] + "</a></td></tr>")
 				//$('#rlinks_holder').append("<li class='rlinks_content'><a href='" + v[4] + "'>" + v[5] + "</a> <span class='rlinks_score'>[" + v[3] + "]</span> <a href='#thing_" + v[1] + "' class='rlinks_author'>#" + v[2] + "</a> </li>")
 			})
 
